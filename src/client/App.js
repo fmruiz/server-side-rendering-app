74d866a018d72a6f2b35a843a4cd998d5396100b
fmruiz <x.francoruiz@gmail.com>
2022-11-22 02:04:46 -0300

    add react config and webpack base config

--- src/client/App.js
+++ src/client/App.js
@@ -5,6 +5,7 @@ const App = () => {
     return (
         <div>
             <Home />
+            <button onClick={() => console.log('xxx')}>ss</button>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const React = require('react');
-const renderToString = require('react-dom/server').renderToString;
-const App = require('./client/App').default;
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from './client/App';
 
 /**
  * Express initialization
@@ -9,10 +9,25 @@ const App = require('./client/App').default;
 const app = express();
 const port = process.env.PORT || 8081;
 
+/**
+ * Middlewares
+ */
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     const content = renderToString(<App />);
 
-    res.send(content);
+    const html = `
+        <html>
+            <head></head>
+            <body>
+                <div id="root">${content}</div>
+                <script src="bundle.js"></script>
+            </body>
+        </html>
+    `;
+
+    res.send(html);
 });
 
 /**
