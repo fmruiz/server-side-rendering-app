859eebf25f1e8bd15f1c2b32a4a65066c465d88f
fmruiz <x.francoruiz@gmail.com>
2022-11-28 01:24:40 -0300

    add renderer helper

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import App from './client/App';
+import renderer from './helpers/renderer';
 
 /**
  * Express initialization
@@ -15,19 +13,7 @@ const port = process.env.PORT || 8081;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    const content = renderToString(<App />);
-
-    const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root">${content}</div>
-                <script src="bundle.js"></script>
-            </body>
-        </html>
-    `;
-
-    res.send(html);
+    res.send(renderer());
 });
 
 /**
